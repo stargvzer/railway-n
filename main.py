78222da792568612e8b0d182468484a4a7695ce0
stargvzer <stargvzer@gmail.com>
2024-10-07 17:31:34 +0500

    требования

--- main.py
+++ main.py
@@ -13,7 +13,6 @@ bot = telebot.TeleBot(TELEGRAM_TOKEN)
 current_dir = os.path.dirname(os.path.abspath(__file__))
 html_file = os.path.join(current_dir, 'history.html')
 
-
 # Функция для чтения HTML-файла и извлечения сообщений
 def get_messages_from_html():
     try:
@@ -27,7 +26,6 @@ def get_messages_from_html():
     except FileNotFoundError:
         return []
 
-
 # Функция для выбора случайного сообщения
 def get_random_message():
     messages = get_messages_from_html()
@@ -35,24 +33,20 @@ def get_random_message():
         return random.choice(messages)
     return "Не удалось найти сообщения в файле."
 
-
 # Обработчик команды /start
 @bot.message_handler(commands=['start'])
 def handle_start(message):
     random_message = get_random_message()  # Получаем случайное сообщение
     bot.send_message(message.chat.id, random_message)
 
-
-@bot.message_handler(
-    func=lambda message: message.text and (
+# Обработчик сообщений, содержащих команду /meh или ключевое слово
+@bot.message_handler(func=lambda message: message.text and (
         '/meh' in message.text or
         'Танк из озера, скажи свою мудрость' in message.text.lower()
-    )
-)
+    ))
 def handle_request(message):
     random_message = get_random_message()  # Получаем случайное сообщение
     bot.send_message(message.chat.id, random_message)
-
 
 # Функция для отправки случайного сообщения в чат
 def send_random_message():
@@ -60,12 +54,10 @@ def send_random_message():
     random_message = get_random_message()  # Получаем случайное сообщение
     bot.send_message(chat_id, random_message)  # Отправляем сообщение в чат
 
-
 # Функция для планирования отправки сообщений раз в час
 def schedule_messages():
     send_random_message()  # Отправляем сообщение сразу
     threading.Timer(3600, schedule_messages).start()  # Повторяем через 1 час
-
 
 # Запуск бота и планирование сообщений
 if __name__ == '__main__':
