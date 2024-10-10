e95dc76ea9d29b0da258a138bf5e36aa25d8f879
stargvzer <stargvzer@gmail.com>
2024-10-10 23:45:59 +0500

    октрытка от бабуина

--- main.py
+++ main.py
@@ -22,6 +22,46 @@ keep_alive()
 def handle_start(message):
     bot.send_message(message.chat.id, kol.startAnswer)
 
+    # Обработчик команды для отправки случайного стикера по запросу (команда /mehp)
+    @bot.message_handler(commands=['mehp'])
+    def handle_random_sticker(message):
+        chat_id = message.chat.id
+        send_random_sticker(chat_id)
+
+    # Обработчик сообщений для отправки случайного стикера по текстовой команде "открытка от бабуина"
+    @bot.message_handler(func=lambda message: 'открытка от бабуина' in message.text.lower())
+    def handle_baboon_postcard(message):
+        chat_id = message.chat.id
+        send_random_sticker(chat_id)
+
+    # Функция для отправки случайного стикера
+    def send_random_sticker(chat_id):
+        try:
+            # Получаем список всех файлов в директории
+            stickers = [f for f in os.listdir(sticker_folder) if os.path.isfile(os.path.join(sticker_folder, f))]
+
+            if stickers:
+                random_sticker = random.choice(stickers)  # Выбираем случайный стикер
+                sticker_path = os.path.join(sticker_folder, random_sticker)
+
+                # Отправляем стикер
+                with open(sticker_path, 'rb') as sticker_file:
+                    bot.send_sticker(chat_id, sticker_file)
+            else:
+                bot.send_message(chat_id, "Не удалось найти стикеры в папке.")
+        except Exception as e:
+            bot.send_message(chat_id, f"Ошибка при отправке стикера: {str(e)}")
+
+    # Функция для отправки случайного стикера в конференцию раз в час
+    def send_random_sticker_to_conference():
+        chat_id = '-1001507836344'  # Замените на ID вашей конференции
+        send_random_sticker(chat_id)
+
+    # Функция для планирования отправки случайных стикеров раз в час
+    def schedule_sticker_messages():
+        send_random_sticker_to_conference()  # Отправляем стикер сразу
+        threading.Timer(3600, schedule_sticker_messages).start()  # Повторяем через 1 час
+
 # Обработчик сообщений с несколькими условиями
 @bot.message_handler(func=lambda message: message.text and (
         '/meh' in message.text.lower() or
@@ -39,46 +79,6 @@ def handle_request(message):
 
     random_message = kol.random_message()  # Получаем случайное сообщение
     bot.send_message(message.chat.id, f"{prefix}{random_message}")  # Отправляем сообщение с нужной фразой
-
-# Обработчик команды для отправки случайного стикера по запросу (команда /mehp)
-@bot.message_handler(commands=['mehp'])
-def handle_random_sticker(message):
-    chat_id = message.chat.id
-    send_random_sticker(chat_id)
-
-# Обработчик сообщений для отправки случайного стикера по текстовой команде "открытка от бабуина"
-@bot.message_handler(func=lambda message: 'открытка от бабуина' in message.text.lower())
-def handle_baboon_postcard(message):
-    chat_id = message.chat.id
-    send_random_sticker(chat_id)
-
-# Функция для отправки случайного стикера
-def send_random_sticker(chat_id):
-    try:
-        # Получаем список всех файлов в директории
-        stickers = [f for f in os.listdir(sticker_folder) if os.path.isfile(os.path.join(sticker_folder, f))]
-
-        if stickers:
-            random_sticker = random.choice(stickers)  # Выбираем случайный стикер
-            sticker_path = os.path.join(sticker_folder, random_sticker)
-
-            # Отправляем стикер
-            with open(sticker_path, 'rb') as sticker_file:
-                bot.send_sticker(chat_id, sticker_file)
-        else:
-            bot.send_message(chat_id, "Не удалось найти стикеры в папке.")
-    except Exception as e:
-        bot.send_message(chat_id, f"Ошибка при отправке стикера: {str(e)}")
-
-# Функция для отправки случайного стикера в конференцию раз в час
-def send_random_sticker_to_conference():
-    chat_id = '-1001507836344'  # Замените на ID вашей конференции
-    send_random_sticker(chat_id)
-
-# Функция для планирования отправки случайных стикеров раз в час
-def schedule_sticker_messages():
-    send_random_sticker_to_conference()  # Отправляем стикер сразу
-    threading.Timer(3600, schedule_sticker_messages).start()  # Повторяем через 1 час
 
 # Функция для отправки случайного сообщения в чат
 def send_random_message():
