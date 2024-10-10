6465a1dac50a684124c0c76ba4a22960b5053f3d
stargvzer <stargvzer@gmail.com>
2024-10-11 00:45:09 +0500

    открытка от бабуина

--- main.py
+++ main.py
@@ -22,24 +22,6 @@ keep_alive()
 def handle_start(message):
     bot.send_message(message.chat.id, kol.startAnswer)
 
-# Обработчик сообщений с несколькими условиями
-@bot.message_handler(func=lambda message: message.text and (
-        '/meh' in message.text.lower() or
-        'танк из озера, скажи свою мудрость' in message.text.lower() or
-        'мать' in message.text.lower() or
-        'шерст' in message.text.lower() or
-        'мудрость от бабуина' in message.text.lower()  # Добавили новое условие
-))
-def handle_request(message):
-    # Проверяем, какое ключевое слово/фраза была использована
-    if 'мудрость от бабуина' in message.text.lower():
-        prefix = "Шведский бабуин сказал: "  # Фраза для "мудрость от бабуина"
-    else:
-        prefix = "Мудрый танк из озера сказал: "  # Фраза по умолчанию
-
-    random_message = kol.random_message()  # Получаем случайное сообщение
-    bot.send_message(message.chat.id, f"{prefix}{random_message}")  # Отправляем сообщение с нужной фразой
-
 # Обработчик команды для отправки случайного стикера по запросу (команда /mehp)
 @bot.message_handler(commands=['mehp'])
 def handle_random_sticker(message):
@@ -70,6 +52,24 @@ def send_random_sticker(chat_id):
     except Exception as e:
         bot.send_message(chat_id, f"Ошибка при отправке стикера: {str(e)}")
 
+# Обработчик сообщений с несколькими условиями
+@bot.message_handler(func=lambda message: message.text and (
+        '/meh' in message.text.lower() or
+        'танк из озера, скажи свою мудрость' in message.text.lower() or
+        'мать' in message.text.lower() or
+        'шерст' in message.text.lower() or
+        'мудрость от бабуина' in message.text.lower()  # Добавили новое условие
+))
+def handle_request(message):
+    # Проверяем, какое ключевое слово/фраза была использована
+    if 'мудрость от бабуина' in message.text.lower():
+        prefix = "Шведский бабуин сказал: "  # Фраза для "мудрость от бабуина"
+    else:
+        prefix = "Мудрый танк из озера сказал: "  # Фраза по умолчанию
+
+    random_message = kol.random_message()  # Получаем случайное сообщение
+    bot.send_message(message.chat.id, f"{prefix}{random_message}")  # Отправляем сообщение с нужной фразой
+
 # Функция для отправки случайного стикера в конференцию раз в час
 def send_random_sticker_to_conference():
     chat_id = '-1001507836344'  # Замените на ID вашей конференции
@@ -92,7 +92,7 @@ def send_random_message():
 # Функция для планирования отправки сообщений раз в час
 def schedule_messages():
     send_random_message()  # Отправляем сообщение сразу
-    threading.Timer(3600, schedule_messages).start()  # Повторяем через 1 час
+    threading.Timer(3599, schedule_messages).start()  # Повторяем через 1 час
 
 # Запуск бота и планирование сообщений
 if __name__ == '__main__':
