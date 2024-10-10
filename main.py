3539260cc74dfee6547970f3758680a80fae8298
stargvzer <stargvzer@gmail.com>
2024-10-10 23:11:50 +0500

    октрытка от бабуина

--- main.py
+++ main.py
@@ -10,21 +10,17 @@ bot = telebot.TeleBot(kol.token)
 # Путь к папке со стикерами
 sticker_folder = 'stickers'  # Замените на реальный путь к вашей папке со стикерами
 
-
 def keep_alive():
     # Периодический запрос к самому себе, чтобы контейнер не останавливался
     threading.Timer(300, keep_alive).start()  # Каждые 5 минут
     requests.get("https://railway-n-istoriya.up.railway.app")  # Замените на ваш Railway URL
 
-
 keep_alive()
-
 
 # Обработчик команды /start
 @bot.message_handler(commands=['start'])
 def handle_start(message):
     bot.send_message(message.chat.id, kol.startAnswer)
-
 
 # Обработчик сообщений с несколькими условиями
 @bot.message_handler(func=lambda message: message.text and (
@@ -44,20 +40,17 @@ def handle_request(message):
     random_message = kol.random_message()  # Получаем случайное сообщение
     bot.send_message(message.chat.id, f"{prefix}{random_message}")  # Отправляем сообщение с нужной фразой
 
-
 # Обработчик команды для отправки случайного стикера по запросу (команда /mehp)
 @bot.message_handler(commands=['mehp'])
 def handle_random_sticker(message):
     chat_id = message.chat.id
     send_random_sticker(chat_id)
 
-
 # Обработчик сообщений для отправки случайного стикера по текстовой команде "открытка от бабуина"
 @bot.message_handler(func=lambda message: 'открытка от бабуина' in message.text.lower())
 def handle_baboon_postcard(message):
     chat_id = message.chat.id
     send_random_sticker(chat_id)
-
 
 # Функция для отправки случайного стикера
 def send_random_sticker(chat_id):
@@ -77,13 +70,32 @@ def send_random_sticker(chat_id):
     except Exception as e:
         bot.send_message(chat_id, f"Ошибка при отправке стикера: {str(e)}")
 
-
 # Функция для отправки случайного стикера в конференцию раз в час
 def send_random_sticker_to_conference():
     chat_id = '-1001507836344'  # Замените на ID вашей конференции
     send_random_sticker(chat_id)
 
-
 # Функция для планирования отправки случайных стикеров раз в час
 def schedule_sticker_messages():
+    send_random_sticker_to_conference()  # Отправляем стикер сразу
+    threading.Timer(3600, schedule_sticker_messages).start()  # Повторяем через 1 час
 
+# Функция для отправки случайного сообщения в чат
+def send_random_message():
+    chat_id = '-1001507836344'  # Замените на ID вашей группы
+    random_message = kol.random_message()  # Получаем случайное сообщение
+
+    # Добавляем фразу к сообщению
+    message_to_send = f"Мудрый танк из озера сказал: {random_message}"
+    bot.send_message(chat_id, message_to_send)  # Отправляем сообщение в чат
+
+# Функция для планирования отправки сообщений раз в час
+def schedule_messages():
+    send_random_message()  # Отправляем сообщение сразу
+    threading.Timer(3600, schedule_messages).start()  # Повторяем через 1 час
+
+# Запуск бота и планирование сообщений
+if __name__ == '__main__':
+    schedule_sticker_messages()  # Запускаем планирование отправки стикеров раз в час
+    schedule_messages()  # Запускаем планирование сообщений
+    bot.polling(none_stop=True)
